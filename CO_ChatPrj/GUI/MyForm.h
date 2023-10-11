b18fd2873075736b48c16ce2f9ea28beffca2d57
moyasuri <moyasuri.hana@gmail.com>
2023-10-11 11:29:33 +0900

    Home GUI Create_adding Sign up

--- CO_ChatPrj/GUI/MyForm.h
+++ CO_ChatPrj/GUI/MyForm.h
@@ -43,6 +43,7 @@ namespace GUI {
 	private: System::Windows::Forms::Button^ button2;
 	private: System::Windows::Forms::TextBox^ textBox1;
 	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::Button^ button3;
 
 
 
@@ -69,23 +70,24 @@ namespace GUI {
 			this->button2 = (gcnew System::Windows::Forms::Button());
 			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
 			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
 			// label1
 			// 
 			this->label1->AutoSize = true;
 			this->label1->BackColor = System::Drawing::Color::Transparent;
-			this->label1->Font = (gcnew System::Drawing::Font(L"Georgia", 22));
-			this->label1->Location = System::Drawing::Point(75, 486);
+			this->label1->Font = (gcnew System::Drawing::Font(L"Georgia", 24));
+			this->label1->Location = System::Drawing::Point(100, 499);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(134, 43);
+			this->label1->Size = System::Drawing::Size(144, 46);
 			this->label1->TabIndex = 0;
 			this->label1->Text = L"Sign in";
 			// 
 			// button1
 			// 
 			this->button1->Font = (gcnew System::Drawing::Font(L"굴림", 15));
-			this->button1->Location = System::Drawing::Point(716, 558);
+			this->button1->Location = System::Drawing::Point(581, 531);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(172, 47);
 			this->button1->TabIndex = 1;
@@ -98,7 +100,7 @@ namespace GUI {
 			this->label2->AutoSize = true;
 			this->label2->BackColor = System::Drawing::Color::Transparent;
 			this->label2->Font = (gcnew System::Drawing::Font(L"Georgia", 12));
-			this->label2->Location = System::Drawing::Point(304, 499);
+			this->label2->Location = System::Drawing::Point(304, 507);
 			this->label2->Name = L"label2";
 			this->label2->Size = System::Drawing::Size(102, 24);
 			this->label2->TabIndex = 2;
@@ -109,7 +111,7 @@ namespace GUI {
 			this->label3->AutoSize = true;
 			this->label3->BackColor = System::Drawing::Color::Transparent;
 			this->label3->Font = (gcnew System::Drawing::Font(L"Georgia", 12));
-			this->label3->Location = System::Drawing::Point(304, 569);
+			this->label3->Location = System::Drawing::Point(304, 574);
 			this->label3->Name = L"label3";
 			this->label3->Size = System::Drawing::Size(95, 24);
 			this->label3->TabIndex = 3;
@@ -118,7 +120,7 @@ namespace GUI {
 			// button2
 			// 
 			this->button2->Font = (gcnew System::Drawing::Font(L"굴림", 15));
-			this->button2->Location = System::Drawing::Point(913, 558);
+			this->button2->Location = System::Drawing::Point(581, 588);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(172, 47);
 			this->button2->TabIndex = 6;
@@ -130,7 +132,7 @@ namespace GUI {
 			this->textBox1->BackColor = System::Drawing::Color::MintCream;
 			this->textBox1->BorderStyle = System::Windows::Forms::BorderStyle::None;
 			this->textBox1->Font = (gcnew System::Drawing::Font(L"Georgia", 12));
-			this->textBox1->Location = System::Drawing::Point(309, 531);
+			this->textBox1->Location = System::Drawing::Point(309, 539);
 			this->textBox1->Name = L"textBox1";
 			this->textBox1->Size = System::Drawing::Size(201, 23);
 			this->textBox1->TabIndex = 7;
@@ -140,10 +142,20 @@ namespace GUI {
 			this->textBox2->BackColor = System::Drawing::Color::MintCream;
 			this->textBox2->BorderStyle = System::Windows::Forms::BorderStyle::None;
 			this->textBox2->Font = (gcnew System::Drawing::Font(L"Georgia", 12));
-			this->textBox2->Location = System::Drawing::Point(309, 601);
+			this->textBox2->Location = System::Drawing::Point(309, 604);
 			this->textBox2->Name = L"textBox2";
 			this->textBox2->Size = System::Drawing::Size(201, 23);
 			this->textBox2->TabIndex = 8;
+			// 
+			// button3
+			// 
+			this->button3->Font = (gcnew System::Drawing::Font(L"굴림", 15));
+			this->button3->Location = System::Drawing::Point(875, 531);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(172, 47);
+			this->button3->TabIndex = 9;
+			this->button3->Text = L"Sign up";
+			this->button3->UseVisualStyleBackColor = true;
 			// 
 			// MyForm
 			// 
@@ -152,6 +164,7 @@ namespace GUI {
 			this->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"$this.BackgroundImage")));
 			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
 			this->ClientSize = System::Drawing::Size(1108, 646);
+			this->Controls->Add(this->button3);
 			this->Controls->Add(this->textBox2);
 			this->Controls->Add(this->textBox1);
 			this->Controls->Add(this->button2);
