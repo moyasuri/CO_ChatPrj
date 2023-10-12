bccfe470c157e354fca702ebf6479ca77e8f05c9
moyasuri <moyasuri.hana@gmail.com>
2023-10-12 18:03:11 +0900

    box ,btn name change - ing

--- CO_ChatPrj/GUI/MyForm.cpp
+++ CO_ChatPrj/GUI/MyForm.cpp
@@ -1,10 +1,104 @@
 ﻿#pragma comment(lib, "ws2_32.lib")
+
+#include <WinSock2.h> //Winsock 헤더파일 include. WSADATA 들어있음.
+#include <WS2tcpip.h>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <thread>
+
 #include "MyForm.h"
 
 
 
+#define MAX_SIZE 1024
 using namespace System;
 using namespace System::Windows::Forms;
+
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+
+int chat_recv() {
+    char buf[MAX_SIZE] = { };
+    string msg;
+
+    while (1) {
+        ZeroMemory(&buf, MAX_SIZE);
+        if (recv(client_sock, buf, MAX_SIZE, 0) > 0) {
+            msg = buf;
+            std::stringstream ss(msg);  // 문자열을 스트림화
+            string user;
+            ss >> user; // 스트림을 통해, 문자열을 공백 분리해 변수에 할당. 보낸 사람의 이름만 user에 저장됨.
+            if (user != my_nick) cout << buf << endl; // 내가 보낸 게 아닐 경우에만 출력하도록.
+        }
+        else {
+            cout << "Server Off" << endl;
+            return -1;
+        }
+    }
+}
+
+void CommunicateWithServer() {
+    // WinSock 초기화
+    WSADATA wsa;
+    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0) {
+        std::cout << "Failed to initialize WinSock" << std::endl;
+        return;
+    }
+
+    // 소켓 생성 및 설정
+    client_sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+    SOCKADDR_IN client_addr = {};
+    client_addr.sin_family = AF_INET;
+    client_addr.sin_port = htons(7777);
+    InetPton(AF_INET, TEXT("192.168.67.227"), &client_addr.sin_addr);
+
+    // 서버에 연결
+
+    // myform에서 닉네임을 설정하도록
+    while (1)
+    {
+        if (my_nick != "")
+        {
+            break;
+        }
+
+        // 데이터 송수신 로직
+        while (1) {
+            if (!connect(client_sock, (SOCKADDR*)&client_addr, sizeof(client_addr))) { // 위에 설정한 정보에 해당하는 server로 연결!
+                cout << "Server Connect" << endl;
+                send(client_sock, my_nick.c_str(), my_nick.length(), 0); // 연결에 성공하면 client 가 입력한 닉네임을 서버로 전송
+                break;
+            }
+
+            // cout << "Connecting..." << endl;
+        }
+
+        std::thread th2(chat_recv);
+
+        while (1) {
+            string text;
+            std::getline(cin, text);
+            const char* buffer = text.c_str(); // string형을 char* 타입으로 변환
+            send(client_sock, buffer, strlen(buffer), 0);
+        }
+        th2.join();
+
+
+        closesocket(client_sock);
+        // 연결 종료
+
+
+
+        // WinSock 정리
+        WSACleanup();
+    }
+}
+
 
 
 [STAThread]
@@ -15,6 +109,12 @@ void main(array<String^>^ args) {
 	Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
 	GUI::MyForm form;
+
+    // 통신 작업을 처리할 스레드 생성 및 실행
+    std::thread communicationThread(CommunicateWithServer);
+    communicationThread.detach();  // 메인 스레드와 분리하여 실행
+
+
 	Application::Run(% form);
 
 
--- CO_ChatPrj/GUI/MyForm.h
+++ CO_ChatPrj/GUI/MyForm.h
@@ -1,18 +1,13 @@
 ﻿#pragma once
 #include "SignUp.h"
-
-#include <WinSock2.h> //Winsock 헤더파일 include. WSADATA 들어있음.
-#include <WS2tcpip.h>
-#include <string>
-#include <sstream>
-#include <iostream>
-#include <thread>
 #include <msclr/marshal_cppstd.h>
-
-#define MAX_SIZE 1024
-
+#include <string>
 
 
+static std::string my_nick;
+static SOCKET client_sock;
+static bool enterServer;
+static bool isExit;
 namespace GUI {
 	using namespace std;
 	using namespace System;
@@ -23,8 +18,6 @@ namespace GUI {
 	using namespace System::Drawing;
 	using namespace System::Media;
 
-	SOCKET client_sock;
-	string my_nick;
 
 
 
@@ -36,7 +29,8 @@ namespace GUI {
 
 
 	private: System::Windows::Forms::TextBox^ textBox; // 텍스트 상자를 멤버 변수로 추가
-	private: System::Windows::Forms::TextBox^ textBox2; // 텍스트 상자를 멤버 변수로 추가
+	private: System::Windows::Forms::TextBox^ txtBoxPW;
+		   // 텍스트 상자를 멤버 변수로 추가
 	private: System::Windows::Forms::Form^ newForm;
 
 
@@ -62,26 +56,7 @@ namespace GUI {
 
 
 		}
-	private:
-		int chat_recv(void) {
-			char buf[MAX_SIZE] = { };
-			string msg;
 
-			while (1) {
-				ZeroMemory(&buf, MAX_SIZE);
-				if (recv(client_sock, buf, MAX_SIZE, 0) > 0) {
-					msg = buf;
-					std::stringstream ss(msg);  // 문자열을 스트림화
-					string user;
-					ss >> user; // 스트림을 통해, 문자열을 공백 분리해 변수에 할당. 보낸 사람의 이름만 user에 저장됨.
-					if (user != my_nick) cout << buf << endl; // 내가 보낸 게 아닐 경우에만 출력하도록.
-				}
-				else {
-					cout << "Server Off" << endl;
-					return -1;
-				}
-			}
-		}
 
 	protected:
 		/// <summary>
@@ -95,18 +70,22 @@ namespace GUI {
 			}
 		}
 	private: System::Windows::Forms::Label^ label1;
-	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ btnSignin;
+
 	private: System::Windows::Forms::Label^ label2;
 	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::TextBox^ txtBoxID;
+	private: System::Windows::Forms::Button^ btnExit;
+	private: System::Windows::Forms::Button^ btnSignup;
 
 
 
-	private: System::Windows::Forms::TextBox^ textBox1;
+
+	// private: System::Windows::Forms::TextBox^ textBox2;
 
 
-	private: System::Windows::Forms::TextBox^ textBox2;
-	private: System::Windows::Forms::Button^ button2;
-	private: System::Windows::Forms::Button^ btn_signup;
+
+
 	private: System::Windows::Forms::Button^ button3;
 
 
@@ -132,13 +111,13 @@ namespace GUI {
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
 			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->btnSignin = (gcnew System::Windows::Forms::Button());
 			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->label3 = (gcnew System::Windows::Forms::Label());
-			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
-			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
-			this->button2 = (gcnew System::Windows::Forms::Button());
-			this->btn_signup = (gcnew System::Windows::Forms::Button());
+			this->txtBoxID = (gcnew System::Windows::Forms::TextBox());
+			this->txtBoxPW = (gcnew System::Windows::Forms::TextBox());
+			this->btnExit = (gcnew System::Windows::Forms::Button());
+			this->btnSignup = (gcnew System::Windows::Forms::Button());
 			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
 			// 
@@ -153,17 +132,17 @@ namespace GUI {
 			this->label1->TabIndex = 0;
 			this->label1->Text = L"Sign in";
 			// 
-			// button1
+			// btnSignin
 			// 
-			this->button1->Font = (gcnew System::Drawing::Font(L"Georgia", 14));
-			this->button1->Location = System::Drawing::Point(527, 527);
-			this->button1->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(171, 41);
-			this->button1->TabIndex = 1;
-			this->button1->Text = L"Sign in";
-			this->button1->UseVisualStyleBackColor = true;
-			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			this->btnSignin->Font = (gcnew System::Drawing::Font(L"Georgia", 14));
+			this->btnSignin->Location = System::Drawing::Point(527, 527);
+			this->btnSignin->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->btnSignin->Name = L"btnSignin";
+			this->btnSignin->Size = System::Drawing::Size(171, 41);
+			this->btnSignin->TabIndex = 1;
+			this->btnSignin->Text = L"Sign in";
+			this->btnSignin->UseVisualStyleBackColor = true;
+			this->btnSignin->Click += gcnew System::EventHandler(this, &MyForm::btnSignin_Click);
 			// 
 			// label2
 			// 
@@ -187,55 +166,57 @@ namespace GUI {
 			this->label3->TabIndex = 3;
 			this->label3->Text = L"Password";
 			// 
-			// textBox1
+			// txtBoxID
 			// 
-			this->textBox1->BackColor = System::Drawing::Color::MintCream;
-			this->textBox1->BorderStyle = System::Windows::Forms::BorderStyle::None;
-			this->textBox1->Font = (gcnew System::Drawing::Font(L"Georgia", 12));
-			this->textBox1->ForeColor = System::Drawing::SystemColors::WindowFrame;
-			this->textBox1->Location = System::Drawing::Point(277, 535);
-			this->textBox1->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
-			this->textBox1->Name = L"textBox1";
-			this->textBox1->Size = System::Drawing::Size(201, 23);
-			this->textBox1->TabIndex = 7;
+			this->txtBoxID->BackColor = System::Drawing::Color::MintCream;
+			this->txtBoxID->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->txtBoxID->Font = (gcnew System::Drawing::Font(L"Georgia", 12));
+			this->txtBoxID->ForeColor = System::Drawing::SystemColors::WindowFrame;
+			this->txtBoxID->Location = System::Drawing::Point(277, 535);
+			this->txtBoxID->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->txtBoxID->Name = L"txtBoxID";
+			this->txtBoxID->Size = System::Drawing::Size(201, 23);
+			this->txtBoxID->TabIndex = 7;
 			// 
-			// textBox2
+			// txtBoxPW
 			// 
-			this->textBox2->BackColor = System::Drawing::Color::MintCream;
-			this->textBox2->BorderStyle = System::Windows::Forms::BorderStyle::None;
-			this->textBox2->Font = (gcnew System::Drawing::Font(L"Georgia", 12));
-			this->textBox2->ForeColor = System::Drawing::SystemColors::WindowFrame;
-			this->textBox2->Location = System::Drawing::Point(277, 601);
-			this->textBox2->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
-			this->textBox2->Name = L"textBox2";
-			this->textBox2->Size = System::Drawing::Size(201, 23);
-			this->textBox2->TabIndex = 10;
+			this->txtBoxPW->BackColor = System::Drawing::Color::MintCream;
+			this->txtBoxPW->BorderStyle = System::Windows::Forms::BorderStyle::None;
+			this->txtBoxPW->Font = (gcnew System::Drawing::Font(L"Georgia", 12));
+			this->txtBoxPW->ForeColor = System::Drawing::SystemColors::WindowFrame;
+			this->txtBoxPW->Location = System::Drawing::Point(277, 601);
+			this->txtBoxPW->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->txtBoxPW->MaxLength = 16;
+			this->txtBoxPW->Name = L"txtBoxPW";
+			this->txtBoxPW->PasswordChar = '*';
+			this->txtBoxPW->Size = System::Drawing::Size(201, 23);
+			this->txtBoxPW->TabIndex = 10;
 			// 
-			// button2
+			// btnExit
 			// 
-			this->button2->Font = (gcnew System::Drawing::Font(L"Georgia", 14));
-			this->button2->ForeColor = System::Drawing::Color::DarkSlateBlue;
-			this->button2->Location = System::Drawing::Point(527, 592);
-			this->button2->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
-			this->button2->Name = L"button2";
-			this->button2->Size = System::Drawing::Size(171, 41);
-			this->button2->TabIndex = 11;
-			this->button2->Text = L"Exit";
-			this->button2->UseVisualStyleBackColor = true;
-			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			this->btnExit->Font = (gcnew System::Drawing::Font(L"Georgia", 14));
+			this->btnExit->ForeColor = System::Drawing::Color::DarkSlateBlue;
+			this->btnExit->Location = System::Drawing::Point(527, 592);
+			this->btnExit->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->btnExit->Name = L"btnExit";
+			this->btnExit->Size = System::Drawing::Size(171, 41);
+			this->btnExit->TabIndex = 11;
+			this->btnExit->Text = L"Exit";
+			this->btnExit->UseVisualStyleBackColor = true;
+			this->btnExit->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
 			// 
-			// btn_signup
+			// btnSignup
 			// 
-			this->btn_signup->Font = (gcnew System::Drawing::Font(L"Georgia", 14));
-			this->btn_signup->ForeColor = System::Drawing::SystemColors::Highlight;
-			this->btn_signup->Location = System::Drawing::Point(888, 529);
-			this->btn_signup->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
-			this->btn_signup->Name = L"btn_signup";
-			this->btn_signup->Size = System::Drawing::Size(171, 41);
-			this->btn_signup->TabIndex = 12;
-			this->btn_signup->Text = L"Sign up";
-			this->btn_signup->UseVisualStyleBackColor = true;
-			this->btn_signup->Click += gcnew System::EventHandler(this, &MyForm::btn_signup_Click);
+			this->btnSignup->Font = (gcnew System::Drawing::Font(L"Georgia", 14));
+			this->btnSignup->ForeColor = System::Drawing::SystemColors::Highlight;
+			this->btnSignup->Location = System::Drawing::Point(888, 529);
+			this->btnSignup->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
+			this->btnSignup->Name = L"btnSignup";
+			this->btnSignup->Size = System::Drawing::Size(171, 41);
+			this->btnSignup->TabIndex = 12;
+			this->btnSignup->Text = L"Sign up";
+			this->btnSignup->UseVisualStyleBackColor = true;
+			this->btnSignup->Click += gcnew System::EventHandler(this, &MyForm::btn_signup_Click);
 			// 
 			// button3
 			// 
@@ -255,13 +236,13 @@ namespace GUI {
 			this->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
 			this->ClientSize = System::Drawing::Size(1109, 646);
 			this->Controls->Add(this->button3);
-			this->Controls->Add(this->btn_signup);
-			this->Controls->Add(this->button2);
-			this->Controls->Add(this->textBox2);
-			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->btnSignup);
+			this->Controls->Add(this->btnExit);
+			this->Controls->Add(this->txtBoxPW);
+			this->Controls->Add(this->txtBoxID);
 			this->Controls->Add(this->label3);
 			this->Controls->Add(this->label2);
-			this->Controls->Add(this->button1);
+			this->Controls->Add(this->btnSignin);
 			this->Controls->Add(this->label1);
 			this->DoubleBuffered = true;
 			this->Margin = System::Windows::Forms::Padding(3, 2, 3, 2);
@@ -273,9 +254,8 @@ namespace GUI {
 		}
 #pragma endregion
 
-	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
-	}
 
+	// sign up form을 띄우기.
 	private: System::Void btn_signup_Click(System::Object^ sender, System::EventArgs^ e) {
 		SignUp^ signupForm = gcnew SignUp;
 		signupForm->Show();
@@ -315,72 +295,54 @@ namespace GUI {
 
 
 
-
-		WSADATA wsa;
-
-
-
-		// Winsock를 초기화하는 함수. MAKEWORD(2, 2)는 Winsock의 2.2 버전을 사용하겠다는 의미.
-		// 실행에 성공하면 0을, 실패하면 그 이외의 값을 반환.
-		// 0을 반환했다는 것은 Winsock을 사용할 준비가 되었다는 의미.
-		int code = WSAStartup(MAKEWORD(2, 2), &wsa);
-		if (!code) {
-
-
-
-
-			//cout << "사용할 닉네임 입력 >> ";
-			//cin >> my_nick;
-
-			// PF_INET : 프로토콜 familiy IPv4 인터넷 프로토콜을 사용
-			// SOCK_STREAM: 소켓 유형(socket type)을 나타냅니다. 
-			// SOCK_STREAM은 TCP(Transmission Control Protocol) 기반의 연결 지향형 소켓을 생성함을 의미합니다.
-			// TCP는 신뢰성 있는 데이터 전송을 제공하며, 연결 지향형으로 동작하여 데이터가 순서대로 전달됩니다.
-			// IPPROTO_TCP: 프로토콜(protocol)을 나타냅니다. IPPROTO_TCP는 TCP 프로토콜을 사용함을 의미합니다.
-			client_sock = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP); // 
-
-			// 연결할 서버 정보 설정 부분
-			SOCKADDR_IN client_addr = {};
-			client_addr.sin_family = AF_INET;
-			client_addr.sin_port = htons(7777);
-			InetPton(AF_INET, TEXT("192.168.0.6"), &client_addr.sin_addr);
-
-			// AF_INTE 과 PF_INET은 정확히 동일한 의미
-
-			while (1) {
-				if (!connect(client_sock, (SOCKADDR*)&client_addr, sizeof(client_addr))) { // 위에 설정한 정보에 해당하는 server로 연결!
-					cout << "Server Connect" << endl;
-					send(client_sock, my_nick.c_str(), my_nick.length(), 0); // 연결에 성공하면 client 가 입력한 닉네임을 서버로 전송
-					break;
-				}
-				cout << "Connecting..." << endl;
-			}
-
-			//thread th2(chat_recv);
-
-			while (1) {
-				string text;
-				std::getline(cin, text);
-				const char* buffer = text.c_str(); // string형을 char* 타입으로 변환
-				send(client_sock, buffer, strlen(buffer), 0);
-			}
-			//th2.join();
-			closesocket(client_sock);
-		}
-
-		WSACleanup();
-
-
 	}
 
 	private: System::Void confirmButton_Click(System::Object^ sender, System::EventArgs^ e) {
 
 
 		String^ inputText = textBox->Text;
+		
+		
+		std::string nick = msclr::interop::marshal_as<std::string>(inputText);
+
+		// my_nick에 값 할당
+		my_nick = nick;
 		// 이곳에서 inputText를 사용하거나 저장할 수 있습니다.
 
 		// 새로운 폼 종료
 		newForm->Close();
 	}
-	};
+		
+
+	// Signin button : database 에서 ID : password  일치하는지 확인하는 함수
+	// ID PW 가 일치하지 않을때는 : ID 또는 PW가 일치하지 않습니다는 메세지를 송출
+	// 없으면 없다고 하기
+
+	private: System::Void btnSignin_Click(System::Object^ sender, System::EventArgs^ e) {
+
+		// 텍스트 상자에서 텍스트 가져오기
+		String^ textBoxText = txtBoxID->Text; // textBox는 해당 텍스트 상자의 이름입니다.
+
+		if (!String::IsNullOrEmpty(textBoxText)) {
+			// 텍스트 상자에 텍스트가 비어 있지 않으면 Message Box로 출력
+			MessageBox::Show(textBoxText, "텍스트 상자 내용", MessageBoxButtons::OK, MessageBoxIcon::Information);
+		}
+		else {
+			// 텍스트 상자가 비어 있을 때 메시지를 출력할 수 있습니다.
+			MessageBox::Show("텍스트 상자가 비어 있습니다.", "경고", MessageBoxButtons::OK, MessageBoxIcon::Warning);
+		}
+
+		textBoxText = txtBoxPW->Text; // textBox는 해당 텍스트 상자의 이름입니다.
+
+		if (!String::IsNullOrEmpty(textBoxText)) {
+			// 텍스트 상자에 텍스트가 비어 있지 않으면 Message Box로 출력
+			MessageBox::Show(textBoxText, "텍스트 상자 내용", MessageBoxButtons::OK, MessageBoxIcon::Information);
+		}
+		else {
+			// 텍스트 상자가 비어 있을 때 메시지를 출력할 수 있습니다.
+			MessageBox::Show("텍스트 상자가 비어 있습니다.", "경고", MessageBoxButtons::OK, MessageBoxIcon::Warning);
+		}
+
+	}
+};
 }
