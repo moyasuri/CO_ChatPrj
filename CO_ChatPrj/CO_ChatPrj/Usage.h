fd58892e2c0077be798d3ae0e5491cd64b297d91
moyasuri <moyasuri.hana@gmail.com>
2023-10-17 19:23:26 +0900

    usage add

---
+++ CO_ChatPrj/CO_ChatPrj/Usage.h
@@ -0,0 +1,30 @@
+enum ID
+{
+    e_try_Signin= 11,
+    e_find_ID = 12,
+    e_find_PW = 13,
+    
+};
+
+enum EditInfo
+{
+    e_modify_PW = 21,
+    e_modify_Email = 22,
+    e_modify_PhoneNum = 23,
+    e_modify_Birth = 24,
+    e_modify_NickName = 25,
+    e_modify_Cha = 26,
+    e_modify_JoinRoom = 27,
+};
+
+enum ShortMessage
+{
+    e_message_List = 31,
+    e_message_Read = 32,
+    e_message_Send = 33,
+    e_message_Delete = 34,
+
+};
+
+
+
